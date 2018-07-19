4fa3617eccd36cd04ae89f0f2e6e6ec83ff698de
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2018-07-19 17:51:06 +0200

    Propagate signed membership information

--- src/status_im/group_chats/core.cljs
+++ src/status_im/group_chats/core.cljs
@@ -1,6 +1,9 @@
 (ns status-im.group-chats.core
   (:require
+   [clojure.string :as string]
+   [re-frame.core :as re-frame]
    [status-im.utils.config :as config]
+   [status-im.native-module.core :as native-module]
    [status-im.transport.utils :as transport.utils]
    [status-im.transport.db :as transport.db]
    [status-im.transport.utils :as transport.utils]
@@ -10,60 +13,176 @@
    [status-im.utils.fx :as fx]
    [status-im.chat.models :as models.chat]))
 
-(defn wrap-group-message [cofx chat-id message]
+(defn- parse-response [response-js]
+  (-> response-js
+      js/JSON.parse
+      (js->clj :keywordize-keys true)))
+
+(defn signature-material [{:keys [chat-id admin participants]}]
+  (apply str
+         (concat (sort participants)
+                 admin
+                 chat-id)))
+
+(defn signature-pairs [{:keys [admin signature] :as payload}]
+  (js/JSON.stringify (clj->js [[(signature-material payload)
+                                signature
+                                (subs admin 2)]])))
+
+(defn valid-chat-id?
+  ;; We need to make sure the chat-id ends with the admin pk (and it's not the same).
+  ;; this is due to prevent an attack whereby a non-admin user would
+  ;; send out a message with identical chat-id and themselves as admin to other members,
+  ;; who would then have to trust the first of the two messages received, possibly
+  ;; resulting in a situation where some of the members in the chat trust a different admin.
+  [chat-id admin]
+  (and (string/ends-with? chat-id admin)
+       (not= chat-id admin)))
+
+(defn wrap-group-message
+  "Wrap a group message in a membership update"
+  [cofx chat-id message]
   (when-let [chat (get-in cofx [:db :chats chat-id])]
-    (transport/GroupMembershipUpdate.
-     chat-id
-     (:name chat)
-     (:group-admin chat)
-     (:contacts chat)
-     nil
-     nil
-     message)))
+    (transport/map->GroupMembershipUpdate.
+     {:chat-id      chat-id
+      :chat-name    (:name chat)
+      :admin        (:group-admin chat)
+      :participants (:contacts chat)
+      :signature    (:membership-signature chat)
+      :version      (:membership-version chat)
+      :message      message})))
 
-(defn update-membership [cofx previous-chat {:keys [chat-id chat-name participants leaves signature version]}]
-  (when (< (:membership-version previous-chat)
-           version)
+(fx/defn update-membership
+  "Upsert chat when version is greater or not existing"
+  [cofx previous-chat {:keys [chat-id
+                              chat-name
+                              participants
+                              leaves
+                              admin
+                              signature
+                              version]}]
+  (when
+   (or
+    (nil? previous-chat)
+    (< (:membership-version previous-chat)
+       version))
+
     (models.chat/upsert-chat cofx
-                             {:chat-id chat-id
-                              :membership-version version})))
+                             {:chat-id              chat-id
+                              :name                 chat-name
+                              :is-active            (get previous-chat :is-active true)
+                              :group-chat           true
+                              :group-admin          admin
+                              :contacts             participants
+                              :membership-signature signature
+                              :membership-version   version})))
 
-(defn send-membership-update [cofx payload chat-id]
+(defn send-membership-update
+  "Send a membership update to all participants but the sender"
+  [cofx payload chat-id]
   (let [{:keys [participants]} payload
         {:keys [current-public-key web3]} (:db cofx)]
     (fx/merge
      cofx
-     {:shh/send-group-message {:web3 web3
-                               :src     current-public-key
-
-                               :dsts    (disj participants current-public-key)
+     {:shh/send-group-message {:web3          web3
+                               :src           current-public-key
+                               :dsts          (disj participants current-public-key)
                                :success-event [:transport/set-message-envelope-hash
                                                chat-id
                                                (transport.utils/message-id (:message payload))
                                                :group-user-message]
-                               :payload payload}})))
+                               :payload       payload}})))
 
-(defn handle-group-leave [payload chat-id cofx]
+(defn send-group-leave [payload chat-id cofx]
   (transport.protocol/send cofx
                            {:chat-id       chat-id
                             :payload       payload
                             :success-event [:group/unsubscribe-from-chat chat-id]}))
 
-(fx/defn handle-membership-update [cofx {:keys [chat-id chat-name participants leaves message signature version] :as membership-update} sender-signature]
-  (when config/group-chats-enabled?
-    (let [chat-fx (if-let [previous-chat (get-in cofx [:db :chats chat-id])]
-                    (update-membership cofx previous-chat membership-update)
-                    (models.chat/upsert-chat
-                     cofx
-                     {:chat-id chat-id
-                      :name chat-name
-                      :is-active true
-                      :group-chat true
-                      :group-admin signature
-                      :contacts participants
-                      :membership-version version}))]
-      (if message
-        (fx/merge cofx
-                  chat-fx
-                  #(protocol.message/receive message chat-id sender-signature nil %))
-        chat-fx))))
+(fx/defn handle-membership-update-received
+  "Verify signatures in status-go and act if successful"
+  [cofx membership-update signature]
+  {:group-chats/verify-membership-signature [[membership-update signature]]})
+
+(fx/defn handle-membership-update
+  "Upsert chat and receive message if valid"
+  ;; Care needs to be taken here as chat-id is not coming from a whisper filter
+  ;; so can be manipulated by the sending user.
+  [cofx {:keys [chat-id
+                chat-name
+                participants
+                signature
+                leaves
+                message
+                admin
+                version] :as membership-update}
+   sender-signature]
+  (when (and config/group-chats-enabled?
+             (valid-chat-id? chat-id admin))
+    (let [previous-chat (get-in cofx [:db :chats chat-id])]
+      (fx/merge cofx
+                (update-membership previous-chat membership-update)
+                #(when (and message
+                            ;; don't allow anything but group messages
+                            (instance? transport.protocol/Message message)
+                            (= :group-user-message (:message-type message)))
+                   (protocol.message/receive message chat-id sender-signature nil %))))))
+
+(defn handle-sign-success
+  "Upsert chat and send signed payload to group members"
+  [{:keys [db] :as cofx} {:keys [chat-id] :as group-update}]
+  (let [my-public-key (:current-public-key db)]
+    (fx/merge cofx
+              (models.chat/navigate-to-chat chat-id {})
+              (handle-membership-update group-update my-public-key)
+              #(protocol.message/send group-update chat-id %))))
+
+(defn handle-sign-response
+  "Callback to dispatch on sign response"
+  [payload response-js]
+  (let [{:keys [error signature]} (parse-response response-js)]
+    (if error
+      (re-frame/dispatch [:group-chats.callback/sign-failed  error])
+      (re-frame/dispatch [:group-chats.callback/sign-success (assoc payload :signature signature)]))))
+
+(defn handle-verify-signature-response
+  "Callback to dispatch on verify signature response"
+  [payload sender-signature response-js]
+  (let [{:keys [error]} (parse-response response-js)]
+    (if error
+      (re-frame/dispatch [:group-chats.callback/verify-signature-failed  error])
+      (re-frame/dispatch [:group-chats.callback/verify-signature-success payload sender-signature]))))
+
+(defn sign-membership [payload]
+  (native-module/sign-group-membership (signature-material payload)
+                                       (partial handle-sign-response payload)))
+
+(defn verify-membership-signature [signatures]
+  (doseq [[payload sender-signature] signatures]
+    (native-module/verify-group-membership-signatures (signature-pairs payload)
+                                                      (partial handle-verify-signature-response payload sender-signature))))
+
+(fx/defn create
+  "Format group update message and sign membership"
+  [{:keys [db random-guid-generator] :as cofx} group-name]
+  (let [my-public-key     (:current-public-key db)
+        chat-id           (str (random-guid-generator) my-public-key)
+        selected-contacts (conj (:group/selected-contacts db)
+                                my-public-key)
+        group-update      (transport/map->GroupMembershipUpdate
+                           {:chat-id      chat-id
+                            :chat-name    group-name
+                            :admin        my-public-key
+                            :participants selected-contacts
+                            :version      1})]
+    {:group-chats/sign-membership group-update
+     :db (assoc db :group/selected-contacts #{})}))
+
+(re-frame/reg-fx
+ :group-chats/sign-membership
+ sign-membership)
+
+(re-frame/reg-fx
+ :group-chats/verify-membership-signature
+ (fn [signatures]
+   (verify-membership-signature signatures)))
