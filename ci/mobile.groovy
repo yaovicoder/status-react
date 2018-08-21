3b4fd7de5b7cca0e3572d9e5ef47b49ceb67261a
Jakub Sokołowski <jakub@status.im>
2018-08-21 19:30:57 -0400

    Squashed commit of the following:

--- ci/mobile.groovy
+++ ci/mobile.groovy
@@ -29,4 +29,72 @@ def leinBuild() {
   sh 'lein prod-build'
 }
 
+<<<<<<< HEAD
+=======
+def compileAndroid(e2e = false) {
+  if (e2e) {
+    env.ENVFILE=".env.e2e"
+  }
+  dir('android') {
+    sh './gradlew react-native-android:installArchives'
+    sh './gradlew assembleRelease'
+  }
+  def pkg = "StatusIm-${GIT_COMMIT.take(6)}.apk"
+  sh "cp android/app/build/outputs/apk/release/app-release.apk ${pkg}"
+  return pkg
+}
+
+def bundleAndroid() {
+  withCredentials([
+    string(credentialsId: "SUPPLY_JSON_KEY_DATA", variable: 'GOOGLE_PLAY_JSON_KEY'),
+    string(credentialsId: "SLACK_URL", variable: 'SLACK_URL')
+  ]) {
+    sh 'bundle exec fastlane android nightly'
+  }
+}
+
+def uploadSauceLabs() {
+  env.SAUCE_LABS_APK = "im.status.ethereum-e2e-${GIT_COMMIT.take(6)}.apk"
+  withCredentials([
+    string(credentialsId: 'SAUCE_ACCESS_KEY', variable: 'SAUCE_ACCESS_KEY'),
+    string(credentialsId: 'SAUCE_USERNAME', variable: 'SAUCE_USERNAME'),
+    string(credentialsId: 'GIT_HUB_TOKEN', variable: 'GITHUB_TOKEN'),
+    string(credentialsId: 'SLACK_JENKINS_WEBHOOK', variable: 'SLACK_URL')
+  ]) {
+    sh 'fastlane android saucelabs'
+  }
+  return env.SAUCE_LABS_APK
+}
+
+def compileiOS() {
+  version = readFile("${env.WORKSPACE}/VERSION").trim()
+  build_no = common.tagBuild()
+  withCredentials([
+    string(credentialsId: 'SLACK_URL', variable: 'SLACK_URL'),
+    string(credentialsId: "slave-pass-${env.NODE_NAME}", variable: 'KEYCHAIN_PASSWORD'),
+    string(credentialsId: 'FASTLANE_PASSWORD', variable: 'FASTLANE_PASSWORD'),
+    string(credentialsId: 'APPLE_ID', variable: 'APPLE_ID'),
+    string(credentialsId: 'fastlane-match-password', variable:'MATCH_PASSWORD')
+  ]) {
+    sh "plutil -replace CFBundleShortVersionString  -string ${version} ios/StatusIm/Info.plist"
+    sh "plutil -replace CFBundleVersion -string ${build_no} ios/StatusIm/Info.plist"
+    sh 'fastlane ios nightly'
+  }
+  def pkg = "StatusIm-${GIT_COMMIT.take(6)}.ipa"
+  sh "cp status-adhoc/StatusIm.ipa ${pkg}"
+  return pkg
+}
+
+def uploadiOS() {
+  withCredentials([
+    string(credentialsId: 'diawi-token', variable: 'DIAWI_TOKEN'),
+    string(credentialsId: 'GIT_HUB_TOKEN', variable: 'GITHUB_TOKEN'),
+    string(credentialsId: 'SLACK_JENKINS_WEBHOOK', variable: 'SLACK_URL')
+  ]) {
+    sh 'fastlane android upload_diawi'
+  }
+  diawiUrl = readFile "${env.WORKSPACE}/fastlane/diawi.out"
+  return diawiUrl
+}
+
 return this
