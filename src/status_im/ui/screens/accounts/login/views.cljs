84b6467aa4a6cc0e35c31dd53528a351c5171d4b
Igor Mandrigin <i@mandrigin.ru>
2018-08-24 15:00:59 +0200

    Don't crash when login-screen isn't removed in time.

--- src/status_im/ui/screens/accounts/login/views.cljs
+++ src/status_im/ui/screens/accounts/login/views.cljs
@@ -55,33 +55,43 @@
   (letsubs [{:keys [address photo-path name password error processing save-password]} [:get :accounts/login]
             can-navigate-back? [:can-navigate-back?]
             password-text-input (atom nil)]
+    ;; due to async nature of UI, this function can be called when
+    ;; :accounts/login is already removed
+    ;; (e.g. when the data is removed but we didn't navigate to the next screen)
+    ;; in that case, we will show a basic progress bar.
     [react/keyboard-avoiding-view {:style ast/accounts-view}
      [status-bar/status-bar]
      [login-toolbar can-navigate-back?]
      [components.common/separator]
      [react/view styles/login-view
-      [react/view styles/login-badge-container
-       [account-login-badge photo-path name]
-       [react/view {:style                       styles/password-container
-                    :important-for-accessibility :no-hide-descendants}
-        [text-input/text-input-with-label
-         {:label             (i18n/label :t/password)
-          :placeholder       (i18n/label :t/password)
-          :ref               #(reset! password-text-input %)
-          :auto-focus        can-navigate-back? ;;this needed because keyboard overlays testfairy alert
-          :on-submit-editing #(login-account @password-text-input address password save-password)
-          :on-change-text    #(do
-                                (re-frame/dispatch [:set-in [:accounts/login :password] %])
-                                (re-frame/dispatch [:set-in [:accounts/login :error] ""]))
-          :secure-text-entry true
-          :text              password
-          :error             (when (pos? (count error)) (i18n/label (error-key error)))}]]
-       (when platform/ios?
-         [react/view {:style styles/save-password-checkbox-container}
-          [profile.components/settings-switch-item
-           {:label-kw :t/save-password
-            :value save-password
-            :action-fn #(re-frame/dispatch [:set-in [:accounts/login :save-password] %])}]])]]
+      ;; so, if this component is rendered with no data
+      (if (or (empty? address) (empty? photo-path))
+        ;; we will show an activiy indicator
+        [react/view styles/processing-view
+         [components/activity-indicator {:animating true}]]
+        ;; otherwise, we will render it properly
+        [react/view styles/login-badge-container
+         [account-login-badge photo-path name]
+         [react/view {:style                       styles/password-container
+                      :important-for-accessibility :no-hide-descendants}
+          [text-input/text-input-with-label
+           {:label             (i18n/label :t/password)
+            :placeholder       (i18n/label :t/password)
+            :ref               #(reset! password-text-input %)
+            :auto-focus        can-navigate-back? ;;this needed because keyboard overlays testfairy alert
+            :on-submit-editing #(login-account @password-text-input address password save-password)
+            :on-change-text    #(do
+                                  (re-frame/dispatch [:set-in [:accounts/login :password] %])
+                                  (re-frame/dispatch [:set-in [:accounts/login :error] ""]))
+            :secure-text-entry true
+            :text              password
+            :error             (when (pos? (count error)) (i18n/label (error-key error)))}]]
+         (when platform/ios?
+           [react/view {:style styles/save-password-checkbox-container}
+            [profile.components/settings-switch-item
+             {:label-kw :t/save-password
+              :value save-password
+              :action-fn #(re-frame/dispatch [:set-in [:accounts/login :save-password] %])}]])])]
      (when processing
        [react/view styles/processing-view
         [components/activity-indicator {:animating true}]
