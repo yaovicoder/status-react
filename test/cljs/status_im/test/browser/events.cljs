2a702c0cfc0ac3350823807e4ea831a78c2adced
Eric Dvorsak <eric@dvorsak.fr>
2018-08-26 02:45:03 +0200

    move init events and fns to dedicated namespace

--- test/cljs/status_im/test/browser/events.cljs
+++ test/cljs/status_im/test/browser/events.cljs
@@ -1,7 +1,7 @@
 (ns status-im.test.browser.events
   (:require [cljs.test :refer-macros [deftest is testing]]
             [day8.re-frame.test :refer-macros [run-test-sync]]
-            [status-im.ui.screens.events :as events]
+            [status-im.init.core :as init]
             status-im.ui.screens.db
             status-im.ui.screens.subs
             [re-frame.core :as re-frame]
@@ -13,7 +13,7 @@
 
 (defn test-fixtures []
 
-  (re-frame/reg-fx ::events/init-store #())
+  (re-frame/reg-fx :init/init-store #())
 
   (re-frame/reg-fx :browse #())
   (re-frame/reg-fx :data-store/tx #())
@@ -43,7 +43,7 @@
    :initialize-test
    (fn [cofx [_]]
      (handlers-macro/merge-fx cofx
-                              (events/initialize-db)
+                              (init/initialize-db)
                               (browser/initialize-browsers)
                               (browser/initialize-dapp-permissions)))))
 
