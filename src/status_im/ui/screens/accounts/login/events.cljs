9cf605232361f5addfaaa74367ba3c91d476a7e0
Eric Dvorsak <eric@dvorsak.fr>
2018-08-27 06:51:04 +0200

    refactor login flow
    simplified flow:
    
    - event `:ui/login` is dispatched
    - node is initialized with user config or default config
    - `node.started` signal is received, applying `:login` fx
    - `:callback/login` event is dispatched, account is changed
    in datastore, web-data is cleared
    - `:init/initialize-account` event is dispatched
    
    replace event dispatches by function composition
    
    fix bug in universal links where url to be processed after login
    was never removed

--- src/status_im/ui/screens/accounts/login/events.cljs
+++ src/status_im/ui/screens/accounts/login/events.cljs
@@ -1,65 +1,41 @@
 (ns status-im.ui.screens.accounts.login.events
-  (:require status-im.ui.screens.accounts.login.navigation
-            [re-frame.core :as re-frame]
+  (:require [re-frame.core :as re-frame]
+            [status-im.ui.screens.accounts.login.models :as models]
             [status-im.utils.handlers :as handlers]
-            [status-im.utils.handlers-macro :as handlers-macro]
-            [status-im.ui.screens.accounts.login.models :as models]))
+            [status-im.init.core :as init]))
 
 ;;;; FX
-
-(re-frame/reg-fx :stop-node models/stop-node!)
-
 (re-frame/reg-fx
  :login
- (fn [[address password save-password]]
-   (models/login! address password save-password)))
+ (fn [[address password save-password?]]
+   (models/login! address password save-password?)))
 
 (re-frame/reg-fx
  :clear-web-data
  models/clear-web-data!)
 
 (re-frame/reg-fx
- :change-account
- (fn [[address]]
-   (models/handle-change-account! address)))
+ :data-store/change-account
+ (fn [address]
+   (models/change-account! address)))
 
 ;;;; Handlers
-
 (handlers/register-handler-fx
- :open-login
+ :ui/open-login
  (fn [cofx [_ address photo-path name]]
    (models/open-login address photo-path name cofx)))
 
 (handlers/register-handler-fx
- :do-login
+ :callback/open-login
  (fn [cofx [_ address photo-path name password]]
-   (handlers-macro/merge-fx cofx
-                            (models/navigate-to-login address photo-path name password)
-                            ;; models/login-account takes care about empty password
-                            (models/login-account address password (not (empty? password))))))
+   (models/open-login-callback address photo-path name password cofx)))
 
 (handlers/register-handler-fx
- :login-account-internal
- (fn [cofx [_ address password save-password]]
-   (models/login-account-internal address password save-password cofx)))
-
-(handlers/register-handler-fx
- :start-node
- (fn [cofx [_ address password save-password]]
-   (models/start-node address password save-password cofx)))
-
-(handlers/register-handler-fx
- :login-account
- (fn [cofx [_ address password save-password]]
-   (models/login-account address password save-password cofx)))
-
-(handlers/register-handler-fx
- :login-handler
- (fn [cofx [_ login-result address password save-password]]
-   (models/login-handler login-result address password save-password cofx)))
-
-(handlers/register-handler-fx
- :change-account-handler
+ :ui/login
  (fn [cofx [_ address]]
-   (models/change-account-handler address cofx)))
+   (models/user-login address true cofx)))
 
+(handlers/register-handler-fx
+ :callback/login
+ (fn [cofx [_ login-result address password save-password?]]
+   (models/user-login-callback login-result address password save-password? cofx)))
