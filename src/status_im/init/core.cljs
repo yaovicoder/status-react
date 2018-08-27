df28817d1e49d6834c8a65763dce41aaf30f47ee
Eric Dvorsak <eric@dvorsak.fr>
2018-08-27 06:51:04 +0200

    refactor init flow
    simplified flow:
    
    - event `:ui/login` is dispatched
    - node is initialized with user config or default config
    - `node.started` signal is received, applying `:login` fx
    - `:callback/login` event is dispatched, account is changed
    in datastore, web-data is cleared
    - `:init/initialize-account` event is dispatched
    
    replace event dispatches by function composition
    
    fix bug in universal links where url to be processed after login
    was never removed

--- src/status_im/init/core.cljs
+++ src/status_im/init/core.cljs
@@ -1,25 +1,27 @@
 (ns status-im.init.core
   (:require [re-frame.core :as re-frame]
             [status-im.constants :as constants]
+            [status-im.data-store.realm.core :as realm]
+            [status-im.i18n :as i18n]
             [status-im.models.account :as models.account]
             [status-im.models.browser :as browser]
             [status-im.models.chat :as chat]
             [status-im.models.contacts :as models.contacts]
-            [status-im.utils.keychain.core :as keychain]
             [status-im.models.protocol :as models.protocol]
             [status-im.models.transactions :as transactions]
             [status-im.models.wallet :as models.wallet]
-            [status-im.transport.inbox :as inbox]
-            [status-im.data-store.realm.core :as realm]
-            [status-im.ui.screens.accounts.models :as accounts.models]
+            [status-im.notifications.core :as notifications]
             [status-im.ui.screens.contacts.events :as contacts]
-            [status-im.i18n :as i18n]
             [status-im.ui.screens.db :refer [app-db]]
+            [status-im.ui.screens.navigation :as navigation]
+            [status-im.utils.config :as config]
             [status-im.utils.ethereum.core :as ethereum]
             [status-im.utils.handlers-macro :as handlers-macro]
+            [status-im.utils.keychain.core :as keychain]
             [status-im.utils.platform :as platform]
-            [taoensso.timbre :as log]
-            [status-im.utils.universal-links.core :as universal-links]))
+            [status-im.utils.types :as types]
+            [status-im.utils.universal-links.core :as universal-links]
+            [taoensso.timbre :as log]))
 
 ;; TODO (yenda) move keychain functions to dedicated namespace
 (defn- reset-keychain []
@@ -57,6 +59,57 @@
 (defn initialize-keychain [cofx]
   {:keychain/get-encryption-key [:init/initialize-app]})
 
+(defn- add-custom-bootnodes [config network all-bootnodes]
+  (let [bootnodes (as-> all-bootnodes $
+                    (get $ network)
+                    (vals $)
+                    (map :address $))]
+    (if (seq bootnodes)
+      (assoc config :ClusterConfig {:Enabled   true
+                                    :BootNodes bootnodes})
+      config)))
+
+(defn get-account-network [db address]
+  (get-in db [:accounts/accounts address :network]))
+
+(defn- get-account-node-config [db address]
+  (let [accounts (get db :accounts/accounts)
+        {:keys [network
+                settings
+                bootnodes
+                networks]} (get accounts address)
+        use-custom-bootnodes (get-in settings [:bootnodes network])]
+    (cond-> (get-in networks [network :config])
+      (and
+       config/bootnodes-settings-enabled?
+       use-custom-bootnodes)
+      (add-custom-bootnodes network bootnodes))))
+
+(defn start-node
+  ([cofx]
+   (start-node nil cofx))
+  ([address {:keys [db]}]
+   (let [network     (if address
+                       (get-account-network db address)
+                       (:network db))
+         node-config (if address
+                       (get-account-node-config db address)
+                       (get-in (:networks/networks db) [network :config]))
+         node-config-json (types/clj->json node-config)]
+     {:db              (assoc db
+                              :network network)
+      :init/start-node node-config-json})))
+
+(defn restart-node
+  []
+  {:init/stop-node nil})
+
+(defn initialize-node
+  [address {{:keys [status-node-started?] :as db} :db :as cofx}]
+  (if (not status-node-started?)
+    (start-node address cofx)
+    (restart-node)))
+
 (defn set-device-uuid [device-uuid {:keys [db]}]
   {:db (assoc db :device-UUID device-uuid)})
 
@@ -75,16 +128,9 @@
                        :name name))))
    :notifications/handle-initial-push-notification initial?})
 
-(defn initialize-geth [{:keys [db]}]
-  (when-not (:status-node-started? db)
-    (let [default-networks (:networks/networks db)
-          default-network  (:network db)]
-      {:init/initialize-geth (get-in default-networks [default-network :config])})))
-
 (defn initialize-db
-  "Initialize db to the initial state"
-  [{{:universal-links/keys [url]
-     :push-notifications/keys [initial?]
+  "Initialize db to initial state"
+  [{{:push-notifications/keys [initial?]
      :keys [status-module-initialized? status-node-started?
             network-status network peers-count peers-summary device-UUID]
      :or {network (get app-db :network)}} :db}]
@@ -96,7 +142,6 @@
               :status-module-initialized? (or platform/ios? js/goog.DEBUG status-module-initialized?)
               :status-node-started? status-node-started?
               :network network
-              :universal-links/url url
               :push-notifications/initial? initial?
               :device-UUID device-UUID)})
 
@@ -116,23 +161,31 @@
                               :init/testfairy-alert                    nil}
                              (initialize-db))))
 
+(defn load-accounts [{:keys [db all-accounts]}]
+  (let [accounts (->> all-accounts
+                      (map (fn [{:keys [address] :as account}]
+                             [address account]))
+                      (into {}))]
+    {:db (assoc db :accounts/accounts accounts)}))
+
 (defn after-decryption [cofx]
   (handlers-macro/merge-fx cofx
                            {:network/listen-to-network-status
                             [#(re-frame/dispatch [:network/update-connection-status %])
                              #(re-frame/dispatch [:network/update-network-status %])]}
-                           (initialize-geth)
-                           (accounts.models/load-accounts)
+                           (start-node)
+                           (load-accounts)
                            (initialize-views)))
 
 (defn initialize-account-db [address {:keys [db web3]}]
-  (let [{:keys [accounts/accounts accounts/create contacts/contacts networks/networks
+  (let [{:universal-links/keys [url]
+         :keys [accounts/accounts accounts/create contacts/contacts networks/networks
                 network network-status peers-count peers-summary view-id navigation-stack
                 status-module-initialized? status-node-started? device-UUID
                 push-notifications/initial? semaphores]
          :or   {network (get app-db :network)}} db
         console-contact (get contacts constants/console-chat-id)
-        current-account (accounts address)
+        current-account (get accounts address)
         account-network-id (get current-account :network network)
         account-network (get-in current-account [:networks account-network-id])]
     {:db (cond-> (assoc app-db
@@ -148,20 +201,29 @@
                         :network network
                         :chain (ethereum/network->chain-name account-network)
                         :push-notifications/initial? initial?
+                        :universal-links/url url
                         :peers-summary peers-summary
                         :peers-count peers-count
                         :device-UUID device-UUID
                         :semaphores semaphores
                         :web3 web3)
+           (= view-id :create-account)
+           (assoc-in [:accounts/create :step] :enter-name)
            console-contact
            (assoc :contacts/contacts {constants/console-chat-id console-contact}))}))
 
-(defn initialize-account [address events-after {:keys [web3] :as cofx}]
+(defn login-only-events [address {:keys [db] :as cofx}]
+  (when (not= (:view-id db) :create-account)
+    (handlers-macro/merge-fx cofx
+                             (navigation/navigate-to-clean :home)
+                             (universal-links/process-stored-event)
+                             (notifications/process-stored-event address))))
+
+(defn initialize-account [address {:keys [web3] :as cofx}]
   (handlers-macro/merge-fx cofx
                            {:web3/set-default-account    [web3 address]
                             :web3/fetch-node-version     web3
-                            :notifications/get-fcm-token nil
-                            :dispatch-n                  (or events-after [])}
+                            :notifications/get-fcm-token nil}
                            (initialize-account-db address)
                            (models.protocol/initialize-protocol address)
                            (models.contacts/load-contacts)
@@ -172,17 +234,5 @@
                            (models.wallet/update-wallet)
                            (transactions/run-update)
                            (transactions/start-sync)
-                           (models.account/update-sign-in-time)))
-
-(defn status-node-started [{{:node/keys [after-start] :as db} :db}]
-  ;;TODO (yenda) instead of passing events we can pass effects here and simply return them
-  (merge {:db (assoc db :status-node-started? true)}
-         (when after-start {:dispatch-n [after-start]})))
-
-(defn status-node-stopped [{{:node/keys [after-stop]} :db}]
-  ;;TODO (yenda) instead of passing events we can pass effects here and simply return them
-  (when after-stop {:dispatch-n [after-stop]}))
-
-(defn status-module-initialized [{:keys [db]}]
-  {:db                                (assoc db :status-module-initialized? true)
-   :init/status-module-initialized-fx nil})
+                           (models.account/update-sign-in-time)
+                           (login-only-events address)))
