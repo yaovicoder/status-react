b478fa49666717524663a493d348942da32fc9c6
Igor Mandrigin <i@mandrigin.ru>
2018-08-29 12:17:03 +0200

    debug info

--- ci/common.groovy
+++ ci/common.groovy
@@ -108,6 +108,7 @@ def pkgFilename(type, ext) {
 def githubNotify(message, changeId) {
   withCredentials([string(credentialsId: 'GIT_HUB_TOKEN', variable: 'githubToken')]) {
     def script = "curl -u status-im:" + githubToken + " -H 'Content-Type: application/json'  --data '{\"body\": \"" + 'success' + "\"}' https://api.github.com/repos/status-im/status-react/issues/" + changeId + "/comments"
+    println("running script:\n****\n" + script + "\n****");
     def ghOutput = sh(returnStdout: true, script: script) 
     println("Result of github comment curl: " + ghOutput);
   }
