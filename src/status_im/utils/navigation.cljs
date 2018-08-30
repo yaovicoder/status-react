3ca51267cc0b6f1b27f914b7f543bf90f38015e2
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2018-08-30 14:36:27 +0300

    _

--- src/status_im/utils/navigation.cljs
+++ src/status_im/utils/navigation.cljs
@@ -1,16 +1,29 @@
 (ns status-im.utils.navigation
   (:require [status-im.react-native.js-dependencies :as js-dependencies]))
 
-(def navigation-actions (.-NavigationActions js-dependencies/react-navigation))
-(def navigation-events (.-NavigationEvents js-dependencies/react-navigation))
-(def stack-actions (.-StackActions js-dependencies/react-navigation))
+(def navigation-actions
+  (when (not status-im.utils.platform/desktop?)
+    (.-NavigationActions js-dependencies/react-navigation)))
+
+(def navigation-events
+  (when (not status-im.utils.platform/desktop?)
+    (.-NavigationEvents js-dependencies/react-navigation)))
+
+(def stack-actions
+  (when (not status-im.utils.platform/desktop?)
+    (.-StackActions js-dependencies/react-navigation)))
+
 (def navigator-ref (atom nil))
 
 (defn set-navigator-ref [ref]
   (reset! navigator-ref ref))
 
+(defn can-be-called? []
+  (and @navigator-ref
+       (not status-im.utils.platform/desktop?)))
+
 (defn navigate-to [route]
-  (when @navigator-ref
+  (when (can-be-called?)
     (.dispatch
      @navigator-ref
      (.navigate
@@ -20,7 +33,7 @@
 ;; 'Navigation/REPLACE'
 ;; https://github.com/react-navigation/react-navigation/blob/master/src/routers/StackActions.js#L5
 (defn navigate-replace [route]
-  (when @navigator-ref
+  (when (can-be-called?)
     (.dispatch
      @navigator-ref
      (.replace
@@ -28,10 +41,11 @@
       #js {:routeName (name route)}))))
 
 (defn- navigate [params]
-  (.navigate navigation-actions (clj->js params)))
+  (when (can-be-called?)
+    (.navigate navigation-actions (clj->js params))))
 
 (defn navigate-reset [state]
-  (when @navigator-ref
+  (when (can-be-called?)
     (.dispatch
      @navigator-ref
      (.reset
@@ -39,7 +53,7 @@
       (clj->js state)))))
 
 (defn navigate-back []
-  (when @navigator-ref
+  (when (can-be-called?)
     (.dispatch
      @navigator-ref
      (.back navigation-actions))))
