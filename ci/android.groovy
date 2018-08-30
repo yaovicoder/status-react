851a0725516308158ea51ea71161e4711ae96d4d
Anton Danchenko <ant.danchenko@gmail.com>
2018-08-30 13:46:37 +0300

    apk name is a PR number if build for a PR
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- ci/android.groovy
+++ ci/android.groovy
@@ -38,13 +38,19 @@ def uploadToPlayStore() {
 }
 
 def uploadToSauceLabs() {
-  env.SAUCE_LABS_APK = "im.status.ethereum-e2e-${GIT_COMMIT.take(6)}.apk"
+  def changeId = common.getParentRunEnv('CHANGE_ID')
+  if (changeId != null) {
+    env.SAUCE_LABS_APK = "${changeId}.apk"
+  } else {
+    env.SAUCE_LABS_APK = "im.status.ethereum-e2e-${GIT_COMMIT.take(6)}.apk"
+  }
   withCredentials([
     string(credentialsId: 'SAUCE_ACCESS_KEY', variable: 'SAUCE_ACCESS_KEY'),
     string(credentialsId: 'SAUCE_USERNAME', variable: 'SAUCE_USERNAME'),
   ]) {
     sh 'bundle exec fastlane android saucelabs'
   }
+  print env.SAUCE_LABS_APK
   return env.SAUCE_LABS_APK
 }
 
