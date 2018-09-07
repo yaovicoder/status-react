6f7b23b3b1bed5981126acdc7f793c3d704f06c4
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2018-09-07 11:53:43 +0200

    Replace call to `GenerateConfig` with local JSON. Closes #5739

--- src/status_im/node/models.cljs
+++ src/status_im/node/models.cljs
@@ -1,7 +1,6 @@
 (ns status-im.node.models
   (:require [status-im.utils.config :as config]
             [status-im.utils.types :as types]
-            [clojure.string :as str]
             [taoensso.timbre :as log]
             [status-im.models.fleet :as fleet]))
 
@@ -11,8 +10,7 @@
                     (vals $)
                     (map :address $))]
     (if (seq bootnodes)
-      (assoc config :ClusterConfig {:Enabled   true
-                                    :BootNodes bootnodes})
+      (assoc-in config [:ClusterConfig :BootNodes] bootnodes)
       config)))
 
 (defn- add-log-level [config log-level]
@@ -28,6 +26,8 @@
 
 (defn- get-account-node-config [db address]
   (let [accounts (get db :accounts/accounts)
+        current-fleet-key (fleet/current-fleet db address)
+        current-fleet (current-fleet-key fleet/fleets)
         {:keys [network
                 settings
                 bootnodes
@@ -36,6 +36,21 @@
         log-level (or (:log-level settings)
                       config/log-level-status-go)]
     (cond-> (get-in networks [network :config])
+      :always
+      (assoc :NoDiscovery false
+             :Rendezvous false
+             :ClusterConfig {:Enabled true
+                             :Fleet              (name current-fleet-key)
+                             :BootNodes          (vals (get-in current-fleet [:boot]))
+                             :TrustedMailServers (vals (get-in current-fleet [:mail]))
+                             :StaticNodes        (vals (get-in current-fleet [:whisper]))}
+             :WhisperConfig {:Enabled true
+                             :LightClient true
+                             :MinimumPoW 0.001
+                             :EnableNTPSync true}
+             :RequireTopics {"LES2@d4e56740f876aef8" {:Min 2, :Max 2}
+                             "whisper"               {:Min 2, :Max 2}})
+
       (and
        config/bootnodes-settings-enabled?
        use-custom-bootnodes)
@@ -46,6 +61,14 @@
 
 (defn get-node-config [db network]
   (-> (get-in (:networks/networks db) [network :config])
+      (assoc :NoDiscovery true
+             :Rendezvous false
+             :WhisperConfig {:Enabled true
+                             :LightClient true
+                             :MinimumPoW 0.001
+                             :EnableNTPSync true}
+             :RequireTopics {"LES2@d4e56740f876aef8" {:Min 2, :Max 2}
+                             "whisper" {:Min 2, :Max 2}})
       (add-log-level config/log-level-status-go)))
 
 (defn start
@@ -58,11 +81,10 @@
          node-config (if address
                        (get-account-node-config db address)
                        (get-node-config db network))
-         node-config-json (types/clj->json node-config)
-         fleet (name (fleet/current-fleet db address))]
+         node-config-json (types/clj->json node-config)]
      (log/info "Node config: " node-config-json)
      {:db         (assoc db :network network)
-      :node/start [node-config-json fleet]})))
+      :node/start [node-config-json]})))
 
 (defn restart
   []
