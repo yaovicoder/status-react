d74ef8ce1cd807b88a68e7f43fb8ba1e676a5657
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2018-09-07 11:53:43 +0200

    Replace call to `GenerateConfig` with local JSON. Closes #5739

--- src/status_im/node/core.cljs
+++ src/status_im/node/core.cljs
@@ -12,8 +12,7 @@
                     (vals $)
                     (map :address $))]
     (if (seq bootnodes)
-      (assoc config :ClusterConfig {:Enabled   true
-                                    :BootNodes bootnodes})
+      (assoc-in config [:ClusterConfig :BootNodes] bootnodes)
       config)))
 
 (defn- add-log-level [config log-level]
@@ -24,11 +23,39 @@
            :LogLevel log-level
            :LogEnabled true)))
 
+(defn get-network-genesis-hash-prefix
+  "returns the hex representation of the first 8 bytes of a network's genesis hash"
+  [network]
+  (cond
+    (= network "1") "d4e56740f876aef8"
+    (= network "3") "41941023680923e0"
+    (= network "4") "6341fd3daf94b748"))
+
+(defn get-les-topic
+  "returns discovery v5 topic derived from genesis of the provided network"
+  [network]
+  (let [les-discovery-identifier "LES2@"
+        hash-prefix (get-network-genesis-hash-prefix network)]
+    (when  hash-prefix
+      (str les-discovery-identifier hash-prefix))))
+
+(defn get-topics
+  [network]
+  (let [les-topic (get-les-topic network)]
+    (cond-> {"whisper" {:Min 2, :Max 2}}
+      les-topic (assoc les-topic {:Min 2, :Max 2}))))
+
 (defn get-account-network [db address]
   (get-in db [:accounts/accounts address :network]))
 
+(defn- get-base-node-config [config]
+  (assoc config
+         :Name "StatusIM"))
+
 (defn- get-account-node-config [db address]
   (let [accounts (get db :accounts/accounts)
+        current-fleet-key (fleet/current-fleet db address)
+        current-fleet (current-fleet-key fleet/fleets)
         {:keys [network
                 settings
                 bootnodes
@@ -37,6 +64,22 @@
         log-level (or (:log-level settings)
                       config/log-level-status-go)]
     (cond-> (get-in networks [network :config])
+      :always
+      (get-base-node-config)
+
+      (boolean current-fleet)
+      (assoc :NoDiscovery false
+             :ClusterConfig {:Enabled true
+                             :Fleet              (name current-fleet-key)
+                             :BootNodes          (vals (get-in current-fleet [:boot]))
+                             :TrustedMailServers (vals (get-in current-fleet [:mail]))
+                             :StaticNodes        (vals (get-in current-fleet [:whisper]))}
+             :WhisperConfig {:Enabled true
+                             :LightClient true
+                             :MinimumPoW 0.001
+                             :EnableNTPSync true}
+             :RequireTopics (get-topics network))
+
       (and
        config/bootnodes-settings-enabled?
        use-custom-bootnodes)
@@ -47,6 +90,8 @@
 
 (defn get-node-config [db network]
   (-> (get-in (:networks/networks db) [network :config])
+      (get-base-node-config)
+      (assoc :NoDiscovery true)
       (add-log-level config/log-level-status-go)))
 
 (defn start
@@ -59,11 +104,10 @@
          node-config (if address
                        (get-account-node-config db address)
                        (get-node-config db network))
-         node-config-json (types/clj->json node-config)
-         fleet (name (fleet/current-fleet db address))]
+         node-config-json (types/clj->json node-config)]
      (log/info "Node config: " node-config-json)
      {:db         (assoc db :network network)
-      :node/start [node-config-json fleet]})))
+      :node/start [node-config-json]})))
 
 (defn restart
   []
@@ -77,8 +121,8 @@
 
 (re-frame/reg-fx
  :node/start
- (fn [[config fleet]]
-   (status/start-node config fleet)))
+ (fn [[config]]
+   (status/start-node config)))
 
 (re-frame/reg-fx
  :node/stop
