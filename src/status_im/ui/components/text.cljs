88357fedef29a7d9b7187daa1bd4090882645b5f
Aleksandr Pantiukhov <alwxndr@gmail.com>
2018-09-17 00:31:26 +0200

    [#5529]: Add UI for sharing deep links

--- src/status_im/ui/components/text.cljs
+++ src/status_im/ui/components/text.cljs
@@ -2,13 +2,15 @@
   (:require [status-im.ui.components.react :as react]
             [status-im.utils.platform :as platform]))
 
-(defn- selectable-text [{:keys [value style]}]
+(defn- selectable-text [{:keys [font value style]}]
   (if platform/ios?
-    [react/text-input {:value     value
+    [react/text-input {:font      font
                        :editable  false
                        :multiline true
-                       :style     style}]
+                       :style     style
+                       :value     value}]
     [react/text {:style               style
+                 :font                font
                  :accessibility-label :address-text
                  :selectable          true}
      value]))
