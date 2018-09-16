35b0cebeaeb9a1fb61e2cd8b6c621cb49efd294f
Eric Dvorsak <eric@status.im>
2018-09-16 18:29:48 +0200

    fixup test touchable background

--- src/status_im/ui/components/tooltip/views.cljs
+++ src/status_im/ui/components/tooltip/views.cljs
@@ -1,26 +1,38 @@
 (ns status-im.ui.components.tooltip.views
   (:require-macros [status-im.utils.views :as views])
-  (:require [status-im.ui.components.animation :as animation]
+  (:require [re-frame.core :as re-frame]
+            [status-im.ui.components.animation :as animation]
             [status-im.ui.components.tooltip.animations :as animations]
             [status-im.ui.components.react :as react]
             [status-im.ui.components.icons.vector-icons :as vector-icons]
             [status-im.ui.components.tooltip.styles :as styles]
             [status-im.ui.components.colors :as colors]
             [status-im.ui.components.styles :as components.styles]
-            [reagent.core :as reagent]))
+            [reagent.core :as reagent]
+            [status-im.utils.utils :as utils]))
 
-(views/defview tooltip [label & [{:keys [bottom-value color font-size font-color fading-timeout] :or {bottom-value -30
-                                                                                                      color :white
-                                                                                                      font-size 15
-                                                                                                      font-color components.styles/color-red-2}}]]
+(views/defview tooltip [label & [{:keys [bottom-value color font-size font-color fading-timeout]
+                                  :or {bottom-value -30
+                                       color :white
+                                       font-size 15
+                                       font-color components.styles/color-red-2}}]]
   (views/letsubs [bottom-anim-value (animation/create-value bottom-value)
-                  opacity-value     (animation/create-value 0)]
-    {:component-did-mount (animations/animate-tooltip bottom-value bottom-anim-value opacity-value fading-timeout 10)}
-    [react/view styles/tooltip-container
-     [react/animated-view {:style (styles/tooltip-animated bottom-anim-value opacity-value)}
-      [react/view (styles/tooltip-text-container color)
-       [react/text {:style (styles/tooltip-text font-size font-color)} label]]
-      [vector-icons/icon :icons/tooltip-triangle {:color color :style styles/tooltip-triangle}]]]))
+                  opacity-value     (animation/create-value 0)
+                  show-tooltip?     (reagent/atom true)]
+    {:component-did-mount (do (when fading-timeout
+                                (utils/set-timeout #(reset! show-tooltip? false)
+                                                   (+ fading-timeout 200)))
+                              (animations/animate-tooltip bottom-value
+                                                          bottom-anim-value
+                                                          opacity-value
+                                                          fading-timeout
+                                                          10))}
+    (when @show-tooltip?
+      [react/view styles/tooltip-container
+       [react/animated-view {:style (styles/tooltip-animated bottom-anim-value opacity-value)}
+        [react/view (styles/tooltip-text-container color)
+         [react/text {:style (styles/tooltip-text font-size font-color)} label]]
+        [vector-icons/icon :icons/tooltip-triangle {:color color :style styles/tooltip-triangle}]]])))
 
 (views/defview bottom-tooltip-info [label on-close]
   (views/letsubs [bottom-anim-value (animation/create-value -150)
