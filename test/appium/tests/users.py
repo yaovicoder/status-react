d95ee3f82938718ac4da3a54465320839523d111
Anton Danchenko <ant.danchenko@gmail.com>
2018-09-17 11:50:01 +0300

    wip

---
+++ test/appium/tests/users.py
@@ -0,0 +1,118 @@
+users = dict()
+
+users['A'] = dict()
+users['A']['password'] = "qwerty"
+users['A']['passphrase'] = "heavy earn fence pool drift balcony act coast stairs basic juice hip"
+users['A']['username'] = "Simplistic Personal Isabellinewheatear"
+users['A']['address'] = "c8f8b3376814d0dfe61e4b9eaf7970697c24f6b2"
+users['A']['public_key'] = "0x042bb1943a6516510ecb57356f1dbeeaf928e3260b4d0b8e81e1e139ab9e4be75" \
+                                            "185086bedb27992d554d6e7782515e4c00d022b89d46ea5c208f0e5003c7286a4"
+
+users['B'] = dict()
+users['B']['password'] = "qwerty"
+users['B']['passphrase'] = "subway size dial screen stomach route skin kitchen radar true coffee six"
+users['B']['username'] = "Unselfish Thrifty Racer"
+users['B']['address'] = "85b159bad0d500483b307ec6892c7fa3be591cbc"
+users['B']['public_key'] = "0x04d25784aec35405a870dba2e44e02103bc6a258fe9fa8580bb1da4deeedf8827" \
+                                            "baa3c44def9a4685a7db7a1d30c46d3ca9e7ef46a6ca9037a59791e2b0eca04fd"
+
+users['C'] = dict()
+users['C']['password'] = "qwerty"
+users['C']['passphrase'] = "potato labor object reward minor casino dismiss size flame task winter report"
+users['C']['username'] = "Magnificent Earnest Bandicoot"
+users['C']['address'] = "549b9bcd069d720390a6819ff8b3d9dd252fa3be"
+users['C']['public_key'] = "0x04099019246c7a54e581dcb68d0d78969a76a8753c588a62f2e7f0e1c28f88e574d" \
+                                            "ae7d9cd6db9cdc6318714b2aa2945e1be8e1a5cf0f1f876b0c0627f8c6bfdb4"
+
+users['D'] = dict()
+users['D']['password'] = "qwerty"
+users['D']['passphrase'] = "weekend unable empty crime blind rhythm crunch answer travel coast silly like"
+users['D']['username'] = "Warped Murky Avocet"
+users['D']['address'] = "616874dc6cc2810cdc930dea26496fcf217d58ca"
+users['D']['public_key'] = "0x049191be3d0b0258732a4c3bd66d2e68cf7e536a0a975fd501a3265cba9ff5f7c78" \
+                                            "f51345417e42f56e16926b685f99448ac05b9608b0d42cf65ee58853f104696"
+
+users['E'] = dict()
+users['E']['password'] = "newuniquepassword12"
+users['E']['passphrase'] = "sea ill guard bounce gesture tomato walnut fitness plastic affair oven transfer"
+users['E']['username'] = "Chartreuse Comfortable Spadefoot"
+users['E']['address'] = "f7cb60839c0de25e37be0391c33bb34a8f0f8414"
+users['E']['public_key'] = "0x04d27bda317be9b3d943db5e098b3ab1c19ac8156d706f1d237c000e2ea0f553eec" \
+                                            "09499522623dc686f5d0a3233c7f1f9724849127cedb5511945e12ca3b4d297"
+
+users['F'] = dict()
+users['F']['password'] = "newuniquepassword12"
+users['F']['passphrase'] = "style case lazy pole general section dawn royal slice evoke crowd boat"
+users['F']['username'] = "Happygolucky Zigzag Stoat"
+users['F']['address'] = "a94a2a7584620677d290549a96046e59a18f09ef"
+users['F']['public_key'] = "0x042edc34542022b2017a8e9bbdbd7535fb4bdf98a5474edbe7179c4782dbe902704" \
+                                            "380f4d7146a2f8e432dfb6fead596057782ae96fba82c3541cc67884e3a9224"
+
+users['G'] = dict()
+users['G']['password'] = "qwerty"
+users['G']['passphrase'] = "load other time gadget fury sudden gossip hurry entry curtain jaguar inflict"
+users['G']['username'] = "Unsung Yearly Amethystgemclam"
+users['G']['address'] = "876c331d60aedd175ca78cb6a50eaeb9681a1868"
+users['G']['public_key'] = "0x04e70078c562b4a65f08e57cc336e8cbd66d27f411324158807654ae82ef16d8f0f" \
+                                            "583454ba2c98a2451a9a990ec3efc874dd845d8ba72a49fb9fb0454d0bd6bab"
+
+users['H'] = dict()
+users['H']['password'] = 'qwerty'
+users['H']['passphrase'] = "age three camp tip jump radio copper merry mention top panther motor"
+users['H']['username'] = "Lawngreen Rich Silverfox"
+users['H']['address'] = "762d87f77c88048281ddad78501fcfe0700f08ce"
+users['H']['public_key'] = "0x040ff9ff7c1fd9d325a0762bf36fdd6efce0190a02bba4367afc99f0313404a5a3ed" \
+                                            "f74aa54ebf7221561e0d83728b4f7934b28809d69ba9e7434fc2fe9d4b9bd4"
+
+users['I'] = dict()
+users['I']['password'] = "new_unique_password"
+users['I']['passphrase'] = "six runway asthma blur secret rebuild parent logic horror decline rib buyer"
+users['I']['username'] = "Leading Practical Paradiseflycatcher"
+users['I']['address'] = "0887afe0ee3e1b195f596350bb04bba034514af9"
+users['I']['public_key'] = "0x04026c389530328076016ebcdc3f8e558e2e9e6e6c015adb1a668783ca1b11" \
+                                            "34260afe392967434cdbdf616138cbadcd009642edd62a5275ad0bc2ff460849fb74"
+
+users['J'] = dict()
+users['J']['password'] = "new_unique_password"
+users['J']['passphrase'] = "shrug during breeze marcy install net reopen uniform atom guilt sadness elite"
+users['J']['username'] = "Blank Bowed Chrysomelid"
+users['J']['address'] = "13b36abe0be8fa607ab5ec755f2719e545705490"
+users['J']['public_key'] = "0x04e9299758f894d45d2515b254799cdfbe823d387f46896a52ec12e8f37f771" \
+                                                   "fba9fb49a44f93198e581651be71132f68d6d7cca6d1d9c801edbbaab58641b5dc4"
+
+users['K'] = dict()
+users['K']['password'] = "new_unique_password"
+users['K']['passphrase'] = "purchase ensure mistake crystal person similar shaft family shield clog risk market"
+users['K']['username'] = "Official Scratchy Bernesemountaindog"
+users['K']['address'] = "8dce052ccda2f6f6b555759cee6957e04a6ddf5b"
+users['K']['public_key'] = "0x04fd6db8ae245ca351cb6e24bea29df6d81cb35b7f7a91c68a8b5a0c49da444" \
+                                                   "8cdb356cc9c24b9d813094a86e1fb2dc2bf5252a3f117194bbb3bb29f4befbe898c"
+
+users['L'] = dict()
+users['L']['password'] = "new_unique_password"
+users['L']['passphrase'] = "night grit town donate length zoo meat collect vapor brush topic check"
+users['L']['username'] = "Joyful Complete Crow"
+users['L']['address'] = "1fc705a336a0a2e48eb3c953ce577e591f2767ad"
+users['L']['public_key'] = "0x04b785a16763a8f9a139ea53c2da4dc976a40e3ca288aaff52e3e9527d19150" \
+                                                   "21ea6dda3083f37bc5c6a160028faf16dc9d7f684ed59473830fe422700abf826ea"
+
+users['M'] = dict()
+users['M']['password'] = "qwerty"
+users['M']['passphrase'] = "thank fruit brisk pond opera census grid husband claw sight chunk arena"
+users['M']['username'] = "Astonishing Low Johndory"
+users['M']['public_key'] = "0x047c67e698c27527e4cac58a3e67103f4fe59849287171464088b321f1c323ac4bfbb54" \
+                                           "c87a3f27179be7404cb840f90f92edddb36bcf1bc3d462c79a2d73566f7"
+
+users['N'] = dict()
+users['N']['password'] = "qwerty"
+users['N']['passphrase'] = "label ill slender audit atom love vote snap edit program climb beyond"
+users['N']['username'] = "Milky Major Albatross"
+users['N']['public_key'] = "0x04b5457d527c337083a2d0c0295492239ff98f577875049d4d09cefbe5af3b76c18c48d" \
+                                           "312aafb788b2b9922249819932bbc570a738e5c3b49d5e84ebcdec247cb"
+
+users['O'] = dict()
+users['O']['password'] = "qwerty"
+users['O']['passphrase'] = "you salmon ticket antique spray panther flee neck scale mad trial exile"
+users['O']['username'] = "Optimistic Gigantic Bagworm"
+users['O']['public_key'] = "0x04dcdb5cac266328c41bdb0e33a266544a4ac1f2655a68170e5fe4452baff1a413a1d40" \
+                           "3dba7e295445505ee55ea03ee99cb7d26bee05e6b486a9bdaaf6be73a0b"
