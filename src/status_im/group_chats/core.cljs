510c7df13fd139cfc9bd56356f4f7822aa5ffd6c
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2018-09-25 12:52:27 +0200

    Shuffle things around

--- src/status_im/group_chats/core.cljs
+++ src/status_im/group_chats/core.cljs
@@ -1,8 +1,25 @@
 (ns status-im.group-chats.core
   (:require
    [status-im.utils.handlers-macro :as handlers-macro]
+   [status-im.transport.utils :as transport.utils]
+   [status-im.transport.db :as transport.db]
    [status-im.transport.message.core :as protocol.message]
    [status-im.chat.models :as models.chat]))
+
+(defn init-chat
+  "Initialises chat on protocol layer.
+  If topic is not passed as argument it is derived from `chat-id`"
+  [{:keys [chat-id topic resend?]
+    :or   {topic   (transport.utils/get-topic chat-id)}}
+   {:keys [db]}]
+  {:db (assoc-in db
+                 [:transport/chats chat-id]
+                 (transport.db/create-chat {:topic   topic
+                                            :resend? resend?}))})
+
+(defn- init-chat-if-new [chat-id cofx]
+  (if (nil? (get-in cofx [:db :transport/chats chat-id]))
+    (init-chat {:chat-id chat-id} cofx)))
 
 (defn update-membership [previous-chat {:keys [chat-id chat-name participants leaves signature version]} cofx]
   (when (< (:membership-version previous-chat)
@@ -10,6 +27,17 @@
     (models.chat/upsert-chat {:chat-id chat-id
                               :membership-version version}
                              cofx)))
+
+(defn send-membership-update [payload chat-id cofx]
+  (let [{:keys [participants]} payload
+        {:keys [current-public-key web3]} (:db cofx)]
+    (handlers-macro/merge-fx
+     cofx
+     {:shh/send-group-message {:web3 web3
+                               :src     current-public-key
+                               :dsts    (disj participants current-public-key)
+                               :payload payload}}
+     (init-chat-if-new chat-id))))
 
 (defn handle-membership-update [{:keys [chat-id chat-name participants leaves message signature version] :as membership-update} sender-signature cofx]
   (let [chat-fx (if-let [previous-chat (get-in cofx [:db :chats chat-id])]
