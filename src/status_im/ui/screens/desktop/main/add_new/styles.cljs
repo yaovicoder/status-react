b74a88875771adfdd89c6af840e3dec1bdf2667f
Vitaliy Vlasov <siphiuel@gmail.com>
2018-09-26 01:24:08 +0300

    Fix chat/topic validation
    Remove empty line
    
    Fix formatting

--- src/status_im/ui/screens/desktop/main/add_new/styles.cljs
+++ src/status_im/ui/screens/desktop/main/add_new/styles.cljs
@@ -84,3 +84,28 @@
    :width         5
    :height        16
    :margin-bottom -16})
+(def tooltip-container
+  {:position    :absolute
+   :align-items :center
+   :align-self  :center
+   :top         -34})
+
+(def tooltip-icon-text
+  {:justify-content  :center
+   :align-items      :center
+   :flex 1
+   :height           24
+   :border-radius    8
+   :padding-left     10
+   :padding-right    10
+   :background-color colors/red-light})
+
+(def tooltip-triangle
+  {:width              0
+   :height             0
+   :border-top-width   9.1
+   :border-left-width  9.1
+   :border-right-width 9.1
+   :border-left-color  :transparent
+   :border-right-color :transparent
+   :border-top-color   colors/red-light})
