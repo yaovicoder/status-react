7d7611649ec90f51f84a97269a92b47e7f160004
yenda <eric@status.im>
2018-09-28 23:03:27 +0200

    validate incoming message
    - add validate method to StatusMessage protocol
    - spec all message types for use in validate method
    - use valid method after transit/decode step to reject
    invalid messages

--- src/status_im/transport/db.cljs
+++ src/status_im/transport/db.cljs
@@ -1,6 +1,7 @@
 (ns ^{:doc "DB spec and utils for the transport layer"}
  status-im.transport.db
-  (:require-macros [status-im.utils.db :refer [allowed-keys]])
+  (:require-macros [status-im.utils.db :refer [allowed-keys]]
+                   [clojure.string :as s])
   (:require [cljs.spec.alpha :as spec]))
 
 ;; required
@@ -35,3 +36,41 @@
    :fetch-history?        true
    :resend?               resend?
    :topic                 topic})
+
+(spec/def ::profile-image :contact/photo-path)
+
+(spec/def :chat/name (spec/nilable string?))
+
+(spec/def :group-chat/admin :global/public-key)
+(spec/def :group-chat/participants (spec/coll-of :global/public-key :kind set?))
+(spec/def :group-chat/signature string?)
+
+(spec/def ::content (spec/conformer (fn [content]
+                                      (cond
+                                        (string? content) {:text content}
+                                        (map? content) content
+                                        :else :clojure.spec/invalid))))
+(spec/def ::content-type #{"text/plain" "command" "emoji"})
+(spec/def ::message-type #{:group-user-message :public-group-user-message :user-message})
+(spec/def ::clock-value (spec/nilable pos?))
+(spec/def ::timestamp (spec/nilable pos?))
+
+(spec/def :message/id string?)
+(spec/def :message/ids (spec/coll-of :message/id :kind set?))
+
+(spec/def ::message (spec/or :message/contact-request :message/contact-request
+                             :message/contact-update :message/contact-update
+                             :message/contact-request-confirmed :message/contact-request-confirmed
+                             :message/message :message/message
+                             :message/message-seen :message/message-seen
+                             :message/group-membership-update :message/group-membership-update))
+
+(spec/def :message/contact-request (spec/keys :req-un [:contact/name ::profile-image :contact/address :contact/fcm-token]))
+(spec/def :message/contact-update (spec/keys :req-un [:contact/name ::profile-image :contact/address :contact/fcm-token]))
+(spec/def :message/contact-request-confirmed (spec/keys :req-un [:contact/name ::profile-image :contact/address :contact/fcm-token]))
+(spec/def :message/new-contact-key (spec/keys :req-un [::sym-key ::topic ::message]))
+
+(spec/def :message/message (spec/keys :req-un [::content ::content-type ::message-type ::clock-value ::timestamp]))
+(spec/def :message/message-seen (spec/keys :req-un [:message/ids]))
+
+(spec/def :message/group-membership-update (spec/keys :req-un [:chat/chat-id :chat/name :group-chat/admin :group-chat/participants :group-chat/signature :message/message]))
