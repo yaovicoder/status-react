692f7b78f4346d735523bbc08068c2bc7e2b6a32
Max Risuhin <risuhin.max@gmail.com>
2018-09-30 21:48:16 +0300

    Changes rebased
    Signed-off-by: Max Risuhin <risuhin.max@gmail.com>

--- src/status_im/chat/models/message.cljs
+++ src/status_im/chat/models/message.cljs
@@ -74,15 +74,15 @@
   (assoc message :outgoing (= from (:current-public-key db))))
 
 (fx/defn add-message
-  [{:keys [db] :as cofx} batch? {:keys [chat-id message-id clock-value content] :as message} current-chat?]
+  [{:keys [db] :as cofx} batch? {:keys [chat-id message-id clock-value timestamp content from] :as message} current-chat?]
   (let [prepared-message (-> message
                              (prepare-message chat-id current-chat?)
                              (add-outgoing-status cofx))]
     (when (and platform/desktop?
                (not= from (:current-public-key db))
                (get-in db [:account/account :desktop-notifications?])
-               (< (datetime/seconds-ago (datetime/to-date timestamp)) 86400))
-      (#(.sendNotification react/desktop-notification content)))
+               (< (time/seconds-ago (time/to-date timestamp)) 86400))
+      (#(.sendNotification react/desktop-notification (:text content))))
     (let [fx {:db            (cond->
                               (-> db
                                   (update-in [:chats chat-id :messages] assoc message-id prepared-message)
