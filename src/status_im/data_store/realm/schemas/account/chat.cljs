107a4575274d2b4785c81c9ccec7f734ebc152bb
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2018-10-01 10:47:20 +0200

    WIP

--- src/status_im/data_store/realm/schemas/account/chat.cljs
+++ src/status_im/data_store/realm/schemas/account/chat.cljs
@@ -126,3 +126,41 @@
                                                :default false}
                       :public?                {:type    :bool
                                                :default false}}})
+
+(def v7 {:name       :chat
+         :primaryKey :chat-id
+         :properties {:chat-id                :string
+                      :name                   :string
+                      :color                  {:type    :string
+                                               :default default-chat-color}
+                      :group-chat             {:type    :bool
+                                               :indexed true}
+                      :group-admin            {:type     :string
+                                               :optional true}
+                      :is-active              :bool
+                      :timestamp              :int
+                      :contacts               {:type     "string[]"}
+                      :removed-at             {:type     :int
+                                               :optional true}
+                      :removed-from-at        {:type     :int
+                                               :optional true}
+                      :deleted-at-clock-value {:type :int
+                                               :optional true}
+                      :added-to-at            {:type     :int
+                                               :optional true}
+                      :updated-at             {:type     :int
+                                               :optional true}
+                      :message-overhead       {:type    :int
+                                               :default 0}
+                      :membership-version     {:type :int
+                                               :optional true}
+                      :membership-signature   {:type :string
+                                               :optional true}
+                      :membership-leaves      {:type       :list
+                                               :objectType :membership-leave}
+                      :membership-invitations {:type       :list
+                                               :objectType :membership-invitation}
+                      :debug?                 {:type    :bool
+                                               :default false}
+                      :public?                {:type    :bool
+                                               :default false}}})
