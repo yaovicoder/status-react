3c6e4e47518f6d76c123387672f59aa99f745993
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2018-10-01 11:12:21 +0300

    [#6042] [desktop] fix Caret position is changed when typing fast in input message field

--- src/status_im/ui/screens/desktop/main/chat/views.cljs
+++ src/status_im/ui/screens/desktop/main/chat/views.cljs
@@ -189,6 +189,17 @@
                     :current-public-key current-public-key)]))]]
        [connectivity/error-view]])))
 
+(views/defview send-button []
+  (views/letsubs [{:keys [input-text]} [:get-current-chat]]
+    (let [empty? (= "" input-text)]
+      [react/touchable-highlight {:style    styles/send-button
+                                  :on-press (fn []
+                                              (.clear @inp-ref)
+                                              (.focus @inp-ref)
+                                              (re-frame/dispatch [:chat.ui/send-current-message]))}
+       [react/view {:style (styles/send-icon empty?)}
+        [icons/icon :icons/arrow-left {:style (styles/send-icon-arrow empty?)}]]])))
+
 (views/defview chat-text-input [chat-id input-text]
   (views/letsubs [inp-ref (atom nil)]
     {:should-component-update
@@ -220,15 +231,8 @@
                           :on-change              (fn [e]
                                                     (let [native-event (.-nativeEvent e)
                                                           text         (.-text native-event)]
-                                                      (reagent/set-state component {:empty? (= "" text)})
                                                       (re-frame/dispatch [:chat.ui/set-chat-input-text text])))}]
-       [react/touchable-highlight {:style    styles/send-button
-                                   :on-press (fn []
-                                               (.clear @inp-ref)
-                                               (.focus @inp-ref)
-                                               (re-frame/dispatch [:chat.ui/send-current-message]))}
-        [react/view {:style (styles/send-icon empty?)}
-         [icons/icon :icons/arrow-left {:style (styles/send-icon-arrow empty?)}]]]])))
+       [send-button]])))
 
 (views/defview chat-view []
   (views/letsubs [{:keys [input-text chat-id] :as current-chat} [:get-current-chat]]
