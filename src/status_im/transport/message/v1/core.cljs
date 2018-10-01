aca11fa3532a2220b8259803976777325bbe3c58
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2018-10-01 10:47:20 +0200

    Allow to edit group membership

--- src/status_im/transport/message/v1/core.cljs
+++ src/status_im/transport/message/v1/core.cljs
@@ -3,13 +3,8 @@
             [cljs.spec.alpha :as spec]))
 
 (defrecord GroupMembershipUpdate
-           [chat-id chat-name admin participants leaves version signature message]
+           [chat-id membership-updates message]
   message/StatusMessage
   (validate [this]
     (when (spec/valid? :message/group-membership-update this)
       this)))
-
-(defrecord GroupLeave
-           []
-  message/StatusMessage
-  (validate [this] this))
