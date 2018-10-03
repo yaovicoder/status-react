9acd782d658ec1496582ddabb3dcc0a84965066f
Jakub Sokołowski <jakub@status.im>
2018-10-02 21:40:25 -0400

    test on macos-01
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- scripts/build-desktop.sh
+++ scripts/build-desktop.sh
@@ -238,7 +238,7 @@ function bundleMacOS() {
     ln -sf ../Resources/qt-reportApp.conf Status.app/Contents/MacOS/qt.conf
     otool -l Status.app/Contents/MacOS/reportApp
     install_name_tool -add_rpath "@executable_path/../Frameworks" \
-                      -delete_rpath "$(joinExistingPath "$QT_PATH" 'lib')" \
+                      -delete_rpath "${$QT_PATH}/lib" \
                       'Status.app/Contents/MacOS/reportApp'
     cp -f ../deployment/macos/Info.plist Status.app/Contents
     cp -f ../deployment/macos/status-icon.icns Status.app/Contents/Resources
