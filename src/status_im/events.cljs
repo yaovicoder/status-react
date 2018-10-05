a5607c6a941b6958c03fb72dbbe5305d8208065e
Igor Mandrigin <i@mandrigin.ru>
2018-10-05 15:38:28 +0200

    more timing-proof version of the sign in sequence

--- src/status_im/events.cljs
+++ src/status_im/events.cljs
@@ -8,6 +8,7 @@
             [status-im.accounts.update.core :as accounts.update]
             [status-im.bootnodes.core :as bootnodes]
             [status-im.browser.core :as browser]
+            [status-im.node.core :as node]
             [status-im.browser.permissions :as browser.permissions]
             [status-im.chat.models :as chat]
             [status-im.chat.models.message :as chat.message]
@@ -222,6 +223,15 @@
  :accounts.login.callback/get-user-password-success
  (fn [cofx [_ password]]
    (accounts.login/open-login-callback cofx password)))
+
+;; node module
+
+;; this event usually means that node has started too late
+;; and we need to restart it with the new config
+(handlers/register-handler-fx
+ :node.needs-reinit
+ (fn [cofx [_ address]]
+   (node/initialize cofx address)))
 
 ;; accounts logout module
 
