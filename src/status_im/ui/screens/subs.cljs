4f1e999b9a2e6c9e4b9bd9e9dd83d277bece202a
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2018-10-10 13:48:02 +0200

    Add pairing

--- src/status_im/ui/screens/subs.cljs
+++ src/status_im/ui/screens/subs.cljs
@@ -17,6 +17,7 @@
             status-im.ui.screens.fleet-settings.subs
             status-im.ui.screens.offline-messaging-settings.subs
             status-im.ui.screens.bootnodes-settings.subs
+            status-im.ui.screens.pairing.subs
             status-im.ui.screens.currency-settings.subs
             status-im.ui.screens.browser.subs
             status-im.ui.screens.add-new.new-chat.subs
