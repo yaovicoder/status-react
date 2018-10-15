537c79c3b5b277d4fd5d3e19ec62e0935697ca32
Jakub Sokołowski <jakub@status.im>
2018-10-14 21:00:40 -0400

    remove unused Fastlane keychain unlocking, use sshagent for fastlane
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- ci/ios.groovy
+++ ci/ios.groovy
@@ -18,7 +18,10 @@ def compile(type = 'nightly') {
     string(credentialsId: 'APPLE_ID', variable: 'APPLE_ID'),
     string(credentialsId: 'fastlane-match-password', variable:'MATCH_PASSWORD')
   ]) {
-    sh "bundle exec fastlane ios ${target}"
+    /* necessary for Fastlane match to clone ios-certificates repo */
+    sshagent(credentials: ['status-im-auto-ssh']) {
+      sh "bundle exec fastlane ios ${target}"
+    }
   }
   def pkg = common.pkgFilename(type, 'ipa')
   sh "cp status-adhoc/StatusIm.ipa ${pkg}"
