d8ed4d09d8ece56049c7a708dc56aee3831e3284
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2018-10-15 14:58:54 +0300

    [#6307] fix each message sent before upgrade is shown twice after upgrade from 0.9.29

--- src/status_im/transport/message/protocol.cljs
+++ src/status_im/transport/message/protocol.cljs
@@ -107,11 +107,14 @@
   (receive [this chat-id signature _ cofx]
     {:chat-received-message/add-fx
      [(assoc (into {} this)
-             :message-id (transport.utils/message-id this)
-             :show?      true
-             :chat-id    chat-id
-             :from       signature
-             :js-obj     (:js-obj cofx))]})
+             :message-id            (transport.utils/message-id this)
+             ;; TODO(rasom): remove this condition
+             ;; on when 0.9.29 will not be available for users
+             :message-id-old-format (transport.utils/message-id-old-format this)
+             :show?                 true
+             :chat-id               chat-id
+             :from                  signature
+             :js-obj                (:js-obj cofx))]})
   (validate [this]
     (if (spec/valid? :message/message this)
       this
