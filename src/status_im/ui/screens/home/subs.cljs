aec242355056a4463b20f606ec93badd1fdd9e9d
Igor Mandrigin <i@mandrigin.ru>
2018-10-15 11:16:56 +0200

    Show debug information when LES is selected (in the titlebar).
    To simplify testing and debugging, there is a very simple sync indicator
    (debug) for LES. This UI is not even close to being production and will
    be reworked in the future.

--- src/status_im/ui/screens/home/subs.cljs
+++ src/status_im/ui/screens/home/subs.cljs
@@ -6,3 +6,15 @@
                   :<- [:browser/browsers]
                   (fn [[chats browsers]]
                     (sort-by #(-> % second :timestamp) > (merge chats browsers))))
+
+(re-frame/reg-sub :chain-sync-state
+                  (fn [{:node/keys [chain-sync-state]} _] chain-sync-state))
+
+(re-frame/reg-sub :current-network-uses-rpc?
+                  (fn [db _]
+                    (let [network (get-in db [:account/account :networks (:network db)])]
+                      (get-in network [:config :UpstreamConfig :Enabled]))))
+
+(re-frame/reg-sub :latest-block-number
+                  (fn [{:node/keys [latest-block-number]} _]
+                    (if latest-block-number latest-block-number 0)))
