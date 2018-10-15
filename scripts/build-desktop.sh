97a6fac1ac2629131a248ecb5ee9ddae0b2b4778
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2018-10-15 06:49:44 +0200

    fixup! Windows port using Conan and MXE.

--- scripts/build-desktop.sh
+++ scripts/build-desktop.sh
@@ -163,16 +163,16 @@ function compile() {
             -DEXTERNAL_MODULES_DIR="$EXTERNAL_MODULES_DIR" \
             -DDESKTOP_FONTS="$DESKTOP_FONTS" \
             -DJS_BUNDLE_PATH="$JS_BUNDLE_PATH" \
-            -DCMAKE_CXX_FLAGS:='-DBUILD_FOR_BUNDLE=1'
+            -DCMAKE_CXX_FLAGS:='-DBUILD_FOR_BUNDLE=1' || exit 1
     else
       cmake -Wno-dev \
             -DCMAKE_BUILD_TYPE=Release \
             -DEXTERNAL_MODULES_DIR="$EXTERNAL_MODULES_DIR" \
             -DDESKTOP_FONTS="$DESKTOP_FONTS" \
             -DJS_BUNDLE_PATH="$JS_BUNDLE_PATH" \
-            -DCMAKE_CXX_FLAGS:='-DBUILD_FOR_BUNDLE=1'
+            -DCMAKE_CXX_FLAGS:='-DBUILD_FOR_BUNDLE=1' || exit 1
     fi
-    make -j5
+    make -j5 || exit 1
   popd
 }
 
