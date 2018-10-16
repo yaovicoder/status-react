1c5fe3e417b97a22dba648b875cc2e932327ae8d
Dmitry Novotochinov <dmitry.novot@gmail.com>
2018-10-16 16:23:29 +0300

    [#5038] desktop deep links
    Add support for status-im://chat/public/status type of links

---
+++ modules/react-native-desktop-linking/desktop/desktoplinking.cpp
@@ -0,0 +1,54 @@
+#include "desktoplinking.h"
+#include "bridge.h"
+#include "eventdispatcher.h"
+
+#include <QCoreApplication>
+#include <QDebug>
+#include <QDesktopServices>
+
+namespace {
+struct RegisterQMLMetaType {
+  RegisterQMLMetaType() { qRegisterMetaType<DesktopLinking *>(); }
+} registerMetaType;
+} // namespace
+
+class DesktopLinkingPrivate {
+public:
+  Bridge *bridge = nullptr;
+  double callbackId;
+};
+
+DesktopLinking::DesktopLinking(QObject *parent)
+    : QObject(parent), d_ptr(new DesktopLinkingPrivate) {
+
+  QCoreApplication::instance()->installEventFilter(this);
+  connect(this, &DesktopLinking::urlOpened, this, &DesktopLinking::handleURL);
+}
+
+DesktopLinking::~DesktopLinking() {
+}
+
+void DesktopLinking::setBridge(Bridge *bridge) {
+  Q_D(DesktopLinking);
+  d->bridge = bridge;
+}
+
+QString DesktopLinking::moduleName() { return "DesktopLinking"; }
+
+QList<ModuleMethod *> DesktopLinking::methodsToExport() {
+  return QList<ModuleMethod *>{};
+}
+
+QVariantMap DesktopLinking::constantsToExport() { return QVariantMap(); }
+
+void DesktopLinking::handleURL(const QString url) {
+    Q_D(DesktopLinking);
+    qDebug() << "call of DesktopLinking::handleURL with param path: " << url;
+    d->bridge->invokePromiseCallback(d->callbackId, QVariantList{url});
+}
+
+void DesktopLinking::onUrlOpened(double callbackId) {
+    Q_D(DesktopLinking);
+    d->callbackId = callbackId;
+    qDebug() << "call of DesktopLinking::setCallback with param callbackId: " << callbackId;
+}
