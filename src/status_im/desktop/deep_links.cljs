d97f66af54d25c6ec232ff4c8e8f4f0407932be5
Dmitry Novotochinov <dmitry.novot@gmail.com>
2018-10-17 12:28:35 +0300

    add logger

--- src/status_im/desktop/deep_links.cljs
+++ src/status_im/desktop/deep_links.cljs
@@ -1,10 +1,13 @@
 (ns status-im.desktop.deep-links
   (:require [re-frame.core :as re-frame]
-            [status-im.react-native.js-dependencies :as js-dependencies]))
+            [status-im.react-native.js-dependencies :as js-dependencies]
+            [taoensso.timbre :as log]))
 
 (defn add-event-listener []
   (let [event-emitter (new (.-NativeEventEmitter js-dependencies/react-native)
                            js-dependencies/desktop-linking)]
     (.addListener event-emitter
                   "urlOpened"
-                  #(re-frame/dispatch [:handle-universal-link %]))))
+                  (fn [url]
+                    (log/debug "urlOpened event with url:" url)
+                    (re-frame/dispatch [:handle-universal-link url])))))
