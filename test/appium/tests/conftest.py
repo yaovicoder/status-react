aae8020050579416e42384e9791343601f222d04
Oleksii Lymarenko <alexey.lymarenko@gmail.com>
2018-10-17 16:16:26 +0300

    adapted normal txs tests to work with rinkeby test network

--- test/appium/tests/conftest.py
+++ test/appium/tests/conftest.py
@@ -49,7 +49,7 @@ def pytest_addoption(parser):
                      help='boolean; For creating testrail report per run')
     parser.addoption('--network',
                      action='store',
-                     default='ropsten',
+                     default='rinkeby',
                      help='string; ropsten or rinkeby')
 
     # message reliability
@@ -189,11 +189,23 @@ def pytest_runtest_protocol(item, nextitem):
             return True  # no need to rerun
 
 
-@pytest.fixture(scope="session", autouse=False)
-def faucet_for_senders():
-    network_api = NetworkApi()
+@pytest.fixture(scope="session", autouse=True)
+def faucet_for_senders(request):
+    network = request.config.getoption('network')
+    network_api = NetworkApi(network)
     for user in transaction_senders.values():
         network_api.faucet(address=user['address'])
+
+
+@pytest.fixture(scope='class', autouse=True)
+def set_network_api_class_attribute(request):
+    network = request.config.getoption('network')
+    request.cls.network_api = NetworkApi(network=network)
+
+
+@pytest.fixture(scope='function', autouse=True)
+def force_switching_network_to_rinkeby(request):
+    request.instance.switch_to_rinkeby = True if request.config.getoption('network') == 'rinkeby' else False
 
 
 @pytest.fixture
