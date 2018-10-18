24b61f241608e51ae796d2883ebc4e5b2f09410a
janherich <jan.herich@gmail.com>
2018-10-18 02:05:39 +0200

    Plug-in new text parsing engine

--- test/cljs/status_im/test/chat/models/message_content.cljs
+++ test/cljs/status_im/test/chat/models/message_content.cljs
@@ -9,7 +9,16 @@
            (:metadata (message-content/enrich-content {:text "Some *styling* present"}))))
     (is (= {:bold [[5 14]]
             :tag  [[28 33] [38 43]]}
-           (:metadata (message-content/enrich-content {:text "Some *styling* present with #tag1 and #tag2 as well"}))))))
+           (:metadata (message-content/enrich-content {:text "Some *styling* present with #tag1 and #tag2 as well"})))))
+  (testing "right to left is correctly identified"
+    (is (not (:rtl? (message-content/enrich-content {:text "You are lucky today!"}))))
+    (is (not (:rtl? (message-content/enrich-content {:text "42"}))))
+    (is (not (:rtl? (message-content/enrich-content {:text "You are lucky today! أنت محظوظ اليوم!"}))))
+    (is (not (:rtl? (message-content/enrich-content {:text "۱۲۳۴۵۶۷۸۹"}))))
+    (is (not (:rtl? (message-content/enrich-content {:text "۱۲۳۴۵۶۷۸۹أنت محظوظ اليوم!"}))))
+    (is (:rtl? (message-content/enrich-content {:text "أنت محظوظ اليوم!"})))
+    (is (:rtl? (message-content/enrich-content {:text "أنت محظوظ اليوم! You are lucky today"})))
+    (is (:rtl? (message-content/enrich-content {:text "יש לך מזל היום!"})))))
 
 (deftest build-render-recipe-test
   (testing "Render tree is build from text"
