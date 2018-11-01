a16cc5dad3a146638b598fec811a800d89f380d5
Goran Jovic <goranjovic@gmail.com>
2018-11-01 17:54:58 +0100

    feature #6509 - validating token config against their contracts; moved token info from compile time list to app-db; fixed any discrepancies in existing info

--- src/status_im/ui/screens/db.cljs
+++ src/status_im/ui/screens/db.cljs
@@ -38,6 +38,7 @@
              :app-state                          "active"
              :wallet.transactions                constants/default-wallet-transactions
              :wallet-selected-asset              {}
+             :wallet/all-tokens                  {}
              :prices                             {}
              :peers-count                        0
              :peers-summary                      []
@@ -238,6 +239,7 @@
                                 :desktop/desktop
                                 :dimensions/window
                                 :dapps/permissions
+                                :wallet/all-tokens
                                 :ui/contact
                                 :ui/search
                                 :ui/chat]
