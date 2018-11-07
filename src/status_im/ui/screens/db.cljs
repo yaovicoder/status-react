b1a3884aea86e481b33c54d8b6cd6b03d631b24b
Vitaliy Vlasov <siphiuel@gmail.com>
2018-11-07 21:55:29 +0200

    Fix Figwheel initial props passing

--- src/status_im/ui/screens/db.cljs
+++ src/status_im/ui/screens/db.cljs
@@ -56,6 +56,7 @@
              :chat/last-outgoing-message-sent-at 0
              :chat/spam-messages-frequency       0
              :tooltips                           {}
+             :initial-props                      {}
              :desktop/desktop                    {:tab-view-id :home}
              :dimensions/window                  (dimensions/window)
              :push-notifications/stored          {}
@@ -146,6 +147,7 @@
 
 (spec/def :desktop/desktop (spec/nilable any?))
 (spec/def ::tooltips (spec/nilable any?))
+(spec/def ::initial-props (spec/nilable any?))
 
 ;;;;NETWORK
 
@@ -250,6 +252,7 @@
                                    ::was-modal?
                                    ::rpc-url
                                    ::tooltips
+                                   ::initial-props
                                    ::web3
                                    ::web3-node-version
                                    ::webview-bridge
