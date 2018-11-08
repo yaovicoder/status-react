a9b961e69064949778cd2de13c9c35e163e7e594
Vitaliy Vlasov <siphiuel@gmail.com>
2018-11-08 19:29:37 +0200

    Fix initial props passing

--- src/status_im/node/core.cljs
+++ src/status_im/node/core.cljs
@@ -5,8 +5,6 @@
             [status-im.native-module.core :as status]
             [status-im.utils.config :as config]
             [status-im.utils.types :as types]
-
-            [re-frame.core :as re-frame]
             [status-im.utils.platform :as utils.platform]
             [status-im.utils.utils :as utils]
             [taoensso.timbre :as log]
@@ -57,8 +55,8 @@
 
 (defn- get-base-node-config [config]
   (let [initial-props @(re-frame/subscribe [:initial-props])
-        status-node-port (get initial-props "STATUS_NODE_PORT")
-        status-node-datadir (get initial-props "STATUS_NODE_DATADIR")]
+        status-node-port (get initial-props :STATUS_NODE_PORT)
+        status-node-datadir (get initial-props :STATUS_NODE_DATADIR)]
     (log/debug "### get-base-node-config" initial-props)
     (cond-> (assoc config
                    :Name "StatusIM"
