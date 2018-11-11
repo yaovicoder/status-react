4734707721ae9443fa542e11fb0e1b10cae65b68
yenda <eric@status.im>
2018-11-11 10:47:50 +0100

    [refactor] chat views and chat list on mobile
    - apply refactoring of chats to mobile
    - simplify subscriptions accordingly

--- src/status_im/ui/screens/home/subs.cljs
+++ src/status_im/ui/screens/home/subs.cljs
@@ -1,8 +1,11 @@
 (ns status-im.ui.screens.home.subs
   (:require [re-frame.core :as re-frame]))
 
-(re-frame/reg-sub :home-items
-                  :<- [:get-active-chats]
-                  :<- [:browser/browsers]
-                  (fn [[chats browsers]]
-                    (sort-by #(-> % second :timestamp) > (merge chats browsers))))
+(re-frame/reg-sub
+ :home-items
+ :<- [:chats/active-chats]
+ :<- [:browser/browsers]
+ (fn [[chats browsers]]
+   (sort-by #(-> % second :timestamp)
+            >
+            (merge chats browsers))))
