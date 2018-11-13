4cc07a635d08816804f739a87e72f014b311e6a1
yenda <eric@status.im>
2018-11-13 13:48:28 +0100

    [refactor] move chat.subs fns to chat.db
    Signed-off-by: yenda <eric@status.im>

--- src/status_im/chat/subs.cljs
+++ src/status_im/chat/subs.cljs
@@ -1,16 +1,12 @@
 (ns status-im.chat.subs
   (:require [clojure.string :as string]
             [re-frame.core :refer [reg-sub subscribe]]
-            [status-im.utils.config :as utils.config]
-            [status-im.chat.constants :as chat.constants]
             [status-im.chat.commands.core :as commands]
             [status-im.chat.commands.input :as commands.input]
-            [status-im.utils.datetime :as time]
-            [status-im.utils.platform :as platform]
-            [status-im.utils.gfycat.core :as gfycat]
-            [status-im.i18n :as i18n]
+            [status-im.chat.constants :as chat.constants]
+            [status-im.chat.db :as chat.db]
             [status-im.models.transactions :as transactions]
-            [clojure.set :as clojure.set]))
+            [status-im.utils.platform :as platform]))
 
 (reg-sub :get-chats :chats)
 
@@ -29,29 +25,19 @@
  (fn [[chat-ui-props id]]
    (get chat-ui-props id)))
 
-(defn chat-name [{:keys [group-chat
-                         chat-id
-                         public?
-                         name]}
-                 {contact-name :name}]
-  (cond
-    public?    (str "#" name)
-    group-chat name
-    :else      (or contact-name
-                   (gfycat/generate-gfy chat-id))))
 (reg-sub
  :get-current-chat-name
  :<- [:get-current-chat-contact]
  :<- [:get-current-chat]
  (fn [[contact chat]]
-   (chat-name chat contact)))
+   (chat.db/chat-name chat contact)))
 
 (reg-sub
  :get-chat-name
  :<- [:get-contacts]
  :<- [:get-chats]
  (fn [[contacts chats] [_ chat-id]]
-   (chat-name (get chats chat-id) (get contacts chat-id))))
+   (chat.db/chat-name (get chats chat-id) (get contacts chat-id))))
 
 (reg-sub
  :get-current-chat-ui-prop
@@ -74,29 +60,13 @@
      platform/ios? kb-height
      :default 0)))
 
-(defn active-chats [[contacts chats {:keys [dev-mode?]}]]
-  (reduce (fn [acc [chat-id {:keys [group-chat public? is-active] :as chat}]]
-            (if (and is-active
-                   ;; not a group chat
-                     (or (not (and group-chat (not public?)))
-                       ;; if it's a group chat
-                         (utils.config/group-chats-enabled? dev-mode?)))
-              (assoc acc chat-id (if-let [contact (get contacts chat-id)]
-                                   (-> chat
-                                       (assoc :name (:name contact))
-                                       (assoc :random-name (gfycat/generate-gfy (:public-key contact)))
-                                       (update :tags clojure.set/union (:tags contact)))
-                                   chat))
-              acc))
-          {}
-          chats))
-
 (reg-sub
  :get-active-chats
  :<- [:get-contacts]
  :<- [:get-chats]
  :<- [:account/account]
- active-chats)
+ (fn [[contacts chats account]]
+   (chat.db/active-chats contacts chats account)))
 
 (reg-sub
  :get-chat
@@ -141,110 +111,6 @@
  (fn [{:keys [referenced-messages]}]
    (or referenced-messages {})))
 
-(defn sort-message-groups
-  "Sorts message groups according to timestamp of first message in group"
-  [message-groups messages]
-  (sort-by
-   (comp unchecked-negate :timestamp (partial get messages) :message-id first second)
-   message-groups))
-
-(defn quoted-message-data
-  "Selects certain data from quoted message which must be available in the view"
-  [message-id messages referenced-messages]
-  (when-let [{:keys [from content]} (get messages message-id
-                                         (get referenced-messages message-id))]
-    {:from from
-     :text (:text content)}))
-
-(defn messages-with-datemarks-and-statuses
-  "Converts message groups into sequence of messages interspersed with datemarks,
-  with correct user statuses associated into message"
-  [message-groups messages message-statuses referenced-messages]
-  (mapcat (fn [[datemark message-references]]
-            (into (list {:value datemark
-                         :type  :datemark})
-                  (map (fn [{:keys [message-id timestamp-str]}]
-                         (let [{:keys [content] :as message} (get messages message-id)
-                               quote (some-> (:response-to content)
-                                             (quoted-message-data messages referenced-messages))]
-                           (cond-> (-> message
-                                       (update :content dissoc :response-to)
-                                       (assoc :datemark      datemark
-                                              :timestamp-str timestamp-str
-                                              :user-statuses (get message-statuses message-id)))
-                             quote ;; quoted message reference
-                             (assoc-in [:content :response-to] quote)))))
-                  message-references))
-          message-groups))
-
-(defn- set-previous-message-info [stream]
-  (let [{:keys [display-photo? message-type] :as previous-message} (peek stream)]
-    (conj (pop stream) (assoc previous-message
-                              :display-username? (and display-photo?
-                                                      (not= :system-message message-type))
-                              :first-in-group?   true))))
-
-(defn display-photo? [{:keys [outgoing message-type]}]
-  (or (= :system-message message-type)
-      (and (not outgoing)
-           (not (= :user-message message-type)))))
-
-; any message that comes after this amount of ms will be grouped separately
-(def ^:private group-ms 60000)
-
-(defn add-positional-metadata
-  "Reduce step which adds positional metadata to a message and conditionally
-  update the previous message with :first-in-group?."
-  [{:keys [stream last-outgoing-seen]}
-   {:keys [type message-type from datemark outgoing timestamp] :as message}]
-  (let [previous-message         (peek stream)
-        ; Was the previous message from a different author or this message
-        ; comes after x ms
-        last-in-group?           (or (= :system-message message-type)
-                                     (not= from (:from previous-message))
-                                     (> (- (:timestamp previous-message) timestamp) group-ms))
-        same-direction?          (= outgoing (:outgoing previous-message))
-        ; Have we seen an outgoing message already?
-        last-outgoing?           (and (not last-outgoing-seen)
-                                      outgoing)
-        datemark?                (= :datemark (:type message))
-        ; If this is a datemark or this is the last-message of a group,
-        ; then the previous message was the first
-        previous-first-in-group? (or datemark?
-                                     last-in-group?)
-        new-message              (assoc message
-                                        :display-photo?  (display-photo? message)
-                                        :same-direction? same-direction?
-                                        :last-in-group?  last-in-group?
-                                        :last-outgoing?  last-outgoing?)]
-    {:stream             (cond-> stream
-                           previous-first-in-group?
-                           ; update previuous message if necessary
-                           set-previous-message-info
-
-                           :always
-                           (conj new-message))
-     ; mark the last message sent by the user
-     :last-outgoing-seen (or last-outgoing-seen last-outgoing?)}))
-
-(defn messages-stream
-  "Enhances the messages in message sequence interspersed with datemarks
-  with derived stream context information, like:
-  `:first-in-group?`, `last-in-group?`, `:same-direction?`, `:last?` and `:last-outgoing?` flags."
-  [ordered-messages]
-  (when (seq ordered-messages)
-    (let [initial-message (first ordered-messages)
-          message-with-metadata (assoc initial-message
-                                       :last-in-group? true
-                                       :last? true
-                                       :display-photo? (display-photo? initial-message)
-                                       :last-outgoing? (:outgoing initial-message))]
-      (->> (rest ordered-messages)
-           (reduce add-positional-metadata
-                   {:stream             [message-with-metadata]
-                    :last-outgoing-seen (:last-outgoing? message-with-metadata)})
-           :stream))))
-
 (reg-sub
  :get-current-chat-messages-stream
  :<- [:get-current-chat-messages]
@@ -252,9 +118,9 @@
  :<- [:get-current-chat-message-statuses]
  :<- [:get-current-chat-referenced-messages]
  (fn [[messages message-groups message-statuses referenced-messages]]
-   (-> (sort-message-groups message-groups messages)
-       (messages-with-datemarks-and-statuses messages message-statuses referenced-messages)
-       messages-stream)))
+   (-> (chat.db/sort-message-groups message-groups messages)
+       (chat.db/messages-with-datemarks-and-statuses messages message-statuses referenced-messages)
+       chat.db/messages-stream)))
 
 (reg-sub
  :get-commands-for-chat
@@ -264,26 +130,18 @@
  (fn [[id->command access-scope->command-id chat]]
    (commands/chat-commands id->command access-scope->command-id chat)))
 
-(def ^:private map->sorted-seq (comp (partial map second) (partial sort-by first)))
-
-(defn- available-commands [[commands {:keys [input-text]}]]
-  (->> commands
-       map->sorted-seq
-       (filter (fn [{:keys [type]}]
-                 (when (commands.input/starts-as-command? input-text)
-                   (string/includes? (commands/command-name type) input-text))))))
-
 (reg-sub
  :get-available-commands
  :<- [:get-commands-for-chat]
  :<- [:get-current-chat]
- available-commands)
+ (fn [[commands chat]]
+   (chat.db/available-commands commands chat)))
 
 (reg-sub
  :get-all-available-commands
  :<- [:get-commands-for-chat]
  (fn [commands]
-   (map->sorted-seq commands)))
+   (chat.db/map->sorted-seq commands)))
 
 (reg-sub
  :selected-chat-command
@@ -359,7 +217,7 @@
  (fn [[_ chat-id]]
    (subscribe [:get-chat chat-id]))
  (fn [{:keys [messages message-groups]}]
-   (->> (sort-message-groups message-groups messages)
+   (->> (chat.db/sort-message-groups message-groups messages)
         first
         second
         last
